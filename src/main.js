331eeb991f5081c70f9b8cd3489df2ee58316b41
kuwako <kuwako@leverages.jp>
2016-05-16 19:05:10 +0900

    react 少し動いた？

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 console.log("OK");
+
+import React from "react";
+import ReactDom from "react-dom";
+
+class LikeButton extends React.Component {
+    render() {
+        return (
+            React.createElement(
+                "div",
+                null,
+                "button"
+            )
+        );
+    }
+}
+
+ReactDom.render(
+    React.createElement("LikeButton"),
+    document.getElementById("like-button")
+);
+
